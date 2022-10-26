66589545329bf1d5f37a2bde9edfb3b31a2b6788
superkruger <superkruger@gmail.com>
2022-10-26 17:24:56 +0200

    use cerberus-reactjs library for Permissions component

--- web/src/components/settings/Settings.js
+++ web/src/components/settings/Settings.js
@@ -1,8 +1,7 @@
 import {Route, Routes} from "react-router-dom";
-import App from "../projects/Project";
-import Permissions from "../permissions/Permissions";
 import {useContext} from "react";
 import {AuthContext} from "../../context/AuthContext";
+import {Permissions} from "cerberus-reactjs";
 
 export default function Settings() {
     const authCtx = useContext(AuthContext)
@@ -10,7 +9,7 @@ export default function Settings() {
     return <>
         <Routes>
             <Route exact path="permissions" element={<Permissions
-                cerberusUrl={"http://localhost:8080/api/"}
+                cerberusUrl={"http://localhost:8000/api/"}
                 cerberusToken={authCtx.cerberusToken}
                 accountId={authCtx.user.accountId}
                 resourceId={authCtx.user.accountId}
